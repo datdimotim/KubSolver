a105aa662e92420b6bce33faf48554dfee13773c
dimotim <datdimotim@gmail.com>
2020-03-28 21:11:55 +0300

    unit tests added

---
+++ src/test/java/KubTest.java
@@ -0,0 +1,51 @@
+import com.dimotim.kubSolver.Kub;
+import com.dimotim.kubSolver.KubSolver;
+import com.dimotim.kubSolver.Solution;
+import com.dimotim.kubSolver.solvers.SimpleSolver1;
+import com.dimotim.kubSolver.solvers.SimpleSolver2;
+import com.dimotim.kubSolver.tables.DoubleTables;
+import com.dimotim.kubSolver.tables.SimpleTables;
+import com.dimotim.kubSolver.tables.SymTables;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+
+public class KubTest {
+    @Test
+    public void SymTablesGenerateTest(){
+        SymTables symTables=new SymTables();
+    }
+
+    @Test
+    public void solveSolvedKub(){
+        Kub kub=new Kub(false);
+        Solution solution=kub.solve();
+        checkSolution(kub,solution);
+    }
+
+    @Test
+    public void solveRandomKub(){
+        Kub kub=new Kub(true);
+        Solution solution=kub.solve();
+        checkSolution(kub,solution);
+    }
+
+    @Test
+    public void checkSameResults(){
+        Kub kub=new Kub(true);
+        Solution s1=new KubSolver<>(new SimpleTables(),new SimpleSolver1<>(),new SimpleSolver2<>()).solve(kub);
+        Solution s2=new KubSolver<>(new DoubleTables(),new SimpleSolver1<>(),new SimpleSolver2<>()).solve(kub);
+        Solution s3=new KubSolver<>(new SymTables(),new SimpleSolver1<>(),new SimpleSolver2<>()).solve(kub);
+        Assertions.assertArrayEquals(s1.getHods(),s2.getHods());
+        Assertions.assertArrayEquals(s2.getHods(),s3.getHods());
+    }
+
+    public void checkSolution(Kub kub, Solution solution){
+        for(int hod:solution.getHods()){
+            kub.povorot(hod);
+        }
+        Assertions.assertEquals(kub.getNumberPos(),BigDecimal.ZERO);
+    }
+}
